9663e4ba9250096eb889549d3724c024de852692
Ahmet Aydin <ahm3t35@gmail.com>
2024-03-11 00:04:42 +0300

    input and delete button

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,40 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => {
-        const localPosts = JSON.parse(localStorage.getItem("posts")) || [];
-        setPosts([...localPosts, ...data.slice(0, 15)]);
-        setLoading(false);
-      });
+    const storedPosts = JSON.parse(localStorage.getItem("posts"));
+    if (storedPosts) {
+      setPosts(storedPosts);
+      setLoading(false);
+    } else {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => {
+          const first15Posts = data.slice(0, 15);
+          setPosts(first15Posts);
+          setLoading(false);
+        });
+    }
   }, []);
+
+  const handleDelete = (id) => {
+    const updatedPosts = posts.filter((post) => post.id !== id);
+    setPosts(updatedPosts);
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const lastUserId = Math.max(...posts.map((post) => post.userId));
-    const updatedPost = { ...newPost, userId: lastUserId + 1 };
+    const lastUserId =
+      posts.length > 0 ? Math.max(...posts.map((post) => post.userId)) : 0;
+    const lastId =
+      posts.length > 0 ? Math.max(...posts.map((post) => post.id)) : 0;
+    const updatedPost = { ...newPost, userId: lastUserId + 1, id: lastId + 1 };
     setPosts([updatedPost, ...posts]);
     setNewPost({ title: "", body: "", userId: lastUserId + 1 });
 
-    // Store the posts in localStorage
-    localStorage.setItem("posts", JSON.stringify([updatedPost, ...posts]));
+    const currentPosts = JSON.parse(localStorage.getItem("posts")) || [];
+    const updatedPosts = [updatedPost, ...currentPosts];
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
   };
 
   if (loading) {
@@ -48,13 +64,19 @@ function App() {
             value={newPost.body}
             onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
           />
-          <button type="submit">Add Post</button>
+          <button type="submit">Post Ekle</button>
         </form>
         {posts.map((post) => (
           <div key={post.id} className="card">
             <h2>{post.title}</h2>
             <p>{post.body}</p>
             <p className="author">Kullanıcı: {post.userId}</p>
+            <button
+              className="delete-button"
+              onClick={() => handleDelete(post.id)}
+            >
+              Sil
+            </button>
           </div>
         ))}
       </header>
