cfef772e2f9c172656a249fdb2181857cc161609
Ahmet Aydin <ahm3t35@gmail.com>
2024-03-16 22:13:07 +0300

    dev

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 
+const preloadedState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : {};
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
 });
